bbb49e83b15c3175629a526c6a95ca4f2e8fa329
Cloiw <sircli@hotmail.com>
2019-07-03 16:46:14 -0400

    modificando test

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -3,20 +3,18 @@ const mdLinks = require('../md-links.js');
 
 
 //stats de un directorio
-test("mdLinks, deberia entregar Total Links: 7 Unique Links: 6 para la ruta test/md-files-test con stats true",  () =>{
-   expect(mdLinks.mdLinks("test/md-files-test",{stats:true})).resolves.toEqual("Total Links:"+7+"\n"+"Unique Links:"+6)
+test("mdLinks, deberia entregar un objeto con total: 7 y unique: 6 para la ruta test/md-files-test con stats true",  () =>{
+   expect(mdLinks.mdLinks("test/md-files-test",{stats:true})).resolves.toEqual({total:7,unique:6})
 });
 
 //stats de un archivo
-test("mdLinks, deberia entregar Total Links: 3 Unique Links: 2 para el archivo test-file-1 con stats true",  () =>{
-  expect(mdLinks.mdLinks("test/md-files-test/test-file-1.md",{stats:true})).resolves.toEqual("Total Links:"+3+"\n"+"Unique Links:"+2)
+test("mdLinks, deberia entregar un objeto con total: 3 y unique: 2 para el archivo test-file-1 con stats true",  () =>{
+  expect(mdLinks.mdLinks("test/md-files-test/test-file-1.md",{stats:true})).resolves.toEqual({total:3,unique:2})
 });
 
 //stats y validate de un archivo
-test("mdLinks, deberia entregar Total Links: 4 Ok Links: 3 y Broken Links: 1 para el archivo test-file-2 con validate y stats true", async () =>{
-  await expect(mdLinks.mdLinks("test/md-files-test/test-file-2.md",{validate:true,stats:true})).resolves.toEqual("Total Links: "+4+"\n"+
-  "Ok Links: "+3+"\n"+
-  "Broken Links: "+1)
+test("mdLinks, deberia entregar un objeto con total:4 ok:3 y broken:1 para el archivo test-file-2 con validate y stats true", async () =>{
+  await expect(mdLinks.mdLinks("test/md-files-test/test-file-2.md",{validate:true,stats:true})).resolves.toEqual({"total": 4,"ok":3,"broken":1})
 });
 
 //validate de un archivo
@@ -45,7 +43,7 @@ test("mdLinks, deberia entregar un mensaje de error al no encontrar una ruta val
 //error de argumentos
 test("mdLinks, deberia entregar un mensaje de error al no encontrar argumentos", async () =>{
   await expect(mdLinks.mdLinks(null,null)).rejects.toMatchObject({
-      message: 'Faltan argumentos.'
+      message: 'Faltan argumentos'
   })
 })
 
