01c7549541eb94f1e4d65cae54242e94cc587ba9
Cloiw <sircli@hotmail.com>
2019-07-05 13:57:35 -0400

    md-links msj no links

--- index.js
+++ index.js
@@ -22,12 +22,18 @@ mdLinks.mdLinks(path,options).then(res=>{
     return console.log("Total Links: "+ res.total+"\n"+"Ok Links: "+res.ok+"\n"+"Broken Links: "+res.broken)
   }
   if(options.validate){
+    if(res.length === 0){
+      return console.log("No se encontraron links")
+    }
     let validateLinks = res.map(x=>x.file+"  "+x.href+"  "+x.text.substr(0,40)+"  "+x.status)
     return console.log(validateLinks.join("\n "))
   }
   if(options.stats){
     return console.log("Total Links: "+ res.total+"\n"+"Unique Links: "+res.unique)
   }else{
+    if(res.length === 0){
+      return console.log("No se encontraron links")
+    }
     const resLinks = res.map(x=>x.file+"  "+x.href+"  "+x.text.substr(0,40))
     return console.log(resLinks.join("\n "))
   }
